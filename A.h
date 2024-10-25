8409a1f6a3387cd07e8e34c3f55d3484a9353fed
Otávio Sbampato <135234329+otaviosbampato@users.noreply.github.com>
2024-10-24 21:44:25 -0300

    Update A.h

--- A.h
+++ A.h
@@ -2,48 +2,51 @@
 
 using namespace std;
 
-class A
-{
-private:
-    int A1;
-    float A2;
+class A {
+    private:
+        int A1;
+        float A2;
+    
+    public:
+        // Getters
+        int getA1() const
+        {
+            return A1;
+        }
+    
+        float getA2() const
+        {
+            return A2;
+        }
 
-public:
-    // Getters
-    int getA1() const
-    {
-        return A1;
-    }
+        int getSoma(int a, int b){
+            return a+b;
+        }
 
-    float getA2() const
-    {
-        return A2;
-    }
-
-    // Setters
-    void setA1(int a1)
-    {
-        A1 = a1;
-    }
-
-    void setA2(float a2)
-    {
-        A2 = a2;
-    }
-
-    // Métodos
-    void MA1()
-    {
-        cout << "Método MA1" << endl;
-    }
-
-    void MA2()
-    {
-        cout << "Método MA2" << endl;
-    }
-
-    void MA3()
-    {
-        cout << "Alteração a classe A partir do clone" << endl;
-    }
+        // Setters
+        void setA1(int a1)
+        {
+            A1 = a1;
+        }
+    
+        void setA2(float a2)
+        {
+            A2 = a2;
+        }
+    
+        // Métodos
+        void MA1()
+        {
+            cout << "Método MA1" << endl;
+        }
+    
+        void MA2()
+        {
+            cout << "Método MA2" << endl;
+        }
+    
+        void MA3()
+        {
+            cout << "Alteração a classe A partir do clone" << endl;
+        }
 };
